422e0919a0c6ca279acea4d7810ccafcca2c178b
Frankiness <690626310@qq.com>
2020-12-08 12:40:20 +0800

    no

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,10 +36,7 @@ module.exports = {
         {
           title: 'Docs',
           items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
+            
             {
               label: 'Second Doc',
               to: 'docs/doc2/',
